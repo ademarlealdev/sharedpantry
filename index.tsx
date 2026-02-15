07e371c30c1aa99efd7f0a99143985da2409d5f7
SharedPantry User <user@sharedpantry.local>
2026-02-15 00:09:42 +0000

    chore: Remove an empty comment line from `index.tsx` and add a hash character to `README.md`.

--- index.tsx
+++ index.tsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-//
 
 // Polyfill process for browser environment
 // This ensures that access to process.env doesn't crash the app
